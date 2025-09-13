d229369eeec46eb4dfaee5a2c7b77a0e25862f23
denilsondamacena <denilsonsilvadamacena@gmail.com>
2025-09-13 13:04:25 -0300

    python

--- exercicio7.py
+++ exercicio7.py
@@ -2,4 +2,4 @@
 
 raio=float(input("Digite o valor do raio do círculo: "))
 perimetro=(2*3.14*raio)
-print(f"O perímetro do cícurlo é de {perimetro:.2f}")
+print(f"O perímetro do círculo é de {perimetro:.2f}")
