daa5a7e3b7f9cdd5c082535a553bba7dec6b321a
denilsondamacena <denilsonsilvadamacena@gmail.com>
2025-09-13 13:03:53 -0300

    python

--- exercicio7.py
+++ exercicio7.py
@@ -1,5 +1,5 @@
 '''7. Escreva um programa que calcule o perímetro de um círculo.'''
 
-raio=float(input("Digite o valor do raio do cícurlo: "))
+raio=float(input("Digite o valor do raio do círculo: "))
 perimetro=(2*3.14*raio)
 print(f"O perímetro do cícurlo é de {perimetro:.2f}")
